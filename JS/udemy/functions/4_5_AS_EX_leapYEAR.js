7afa1a4bf6ebd14efbd2e6246e97c30a58ba496e
bennyh960 <bennyh960@gmail.com>
2022-04-06 17:55:44 +0300

    4.5 added

---
+++ JS/udemy/functions/4_5_AS_EX_leapYEAR.js
@@ -0,0 +1,17 @@
+// Create a JS function to determine whether or not a given year is a leap year. 
+// The function should take a year as an argument:
+// 1) Determine whether or not it is a leap year
+// * If the given year is a leap year, the program should print "It is indeed a 
+// leap year", otherwise the program will print "This is not a leap year."
+// Help:
+// every year that is divisible by 4 is a leap year except for the ones that are 
+// also divisible by 100, unless it is divisible by 400. Example: 2012 is a leap 
+// year 2100 is not But 2400 is.
+
+const isItLeapYear =( year => {
+    if( year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0)){
+        return console.log("It is indeed a leap year");
+    }
+    return console.log("This is not a leap year.");
+
+})(200);
