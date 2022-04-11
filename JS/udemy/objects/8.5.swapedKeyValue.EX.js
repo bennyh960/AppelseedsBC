6d426a59ab7245869d869d4995de3204a41ae196
bennyh960 <bennyh960@gmail.com>
2022-04-11 18:40:18 +0300

    added all for objects

---
+++ JS/udemy/objects/8.5.swapedKeyValue.EX.js
@@ -0,0 +1,19 @@
+let myobj = {
+    key1 : 'value1',
+    key2 : 'value2',
+    key3 : 'value3'
+}
+
+
+function swapKV(obj){
+    let tempObj = {};
+    for(let k in obj){
+        tempObj[obj[k]] = k;
+    }
+    // console.log(obj);  
+    return tempObj;
+}
+
+myobj = swapKV(myobj)
+console.log(myobj);
+
