386aadf0bd69969efaa277f57326408d8e30cd06
bennyh960 <bennyh960@gmail.com>
2022-04-12 10:07:20 +0300

    another way for return

--- JS/udemy/objects/8.4.countLetters.EX.js
+++ JS/udemy/objects/8.4.countLetters.EX.js
@@ -13,10 +13,22 @@ function setObjfromArray(arr){
             letterObj[letter] = longWord.split(letter).length - 1; 
         }
     }
-
+    
     console.log(letterObj);
-    let maxValue = Math.max(...Object.values(letterObj));
-    let maxKey  = Object.keys(letterObj).find(key => letterObj[key] === maxValue);
+    
+    let maxValue = 0;
+    let maxKey ='';
+    // simple way to find max key by value 
+    // for(let char in letterObj){
+    //     if(letterObj[char] > maxValue){
+    //         maxKey = char;
+    //         maxValue = letterObj[char];
+    //     }                  
+    // }
+    // return `letter "${maxKey}" is with the most occurrences of ${maxValue} times. `
+    
+    maxValue = Math.max(...Object.values(letterObj));
+    maxKey  = Object.keys(letterObj).find(key => letterObj[key] === maxValue);
     return `letter "${maxKey}" is with the most occurrences of ${maxValue} times. `
 }
 
