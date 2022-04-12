173846ae944127ac375dd0946b63786717a82359
bennyh960 <bennyh960@gmail.com>
2022-04-12 16:33:53 +0300

    new

--- JS/udemy/chapter 9 -loops EX/9.4.steps.js
+++ JS/udemy/chapter 9 -loops EX/9.4.steps.js
@@ -1,3 +1,4 @@
+// without space
 const steps = N => {
     let symbole = '#';
     let arr = [];
@@ -8,7 +9,7 @@ const steps = N => {
     
 }
 
-// steps(5);
+// with spaces and not repeated
 const steps2 = N => {
     let symbole = '#';
     let arr = [];
@@ -19,9 +20,21 @@ const steps2 = N => {
     for(let i = 0 ; i < N ; i++){
         arr.unshift(symbole);
         arr.pop();
-        console.log(arr.join(''));
+        console.log(`"${arr.join('')}"`);
     }
 }
 steps2(5);
 
 
+// with repeat 
+const steps3 = N => {
+    for(let i = 1; i < N ; i++){
+        console.log(`"${'#'.repeat(i) + ' '.repeat(N-i)}"`);
+    }
+}
+
+steps3(5);
+
+
+// another ninja way
+// step4(5) = N => {}
