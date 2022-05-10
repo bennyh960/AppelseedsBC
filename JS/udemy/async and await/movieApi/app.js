7d9f1708f484a1a79c99e449c752de66c95e4e2e
bennyh960 <bennyh960@gmail.com>
2022-05-10 19:25:46 +0300

    movie

---
+++ JS/udemy/async and await/movieApi/app.js
@@ -0,0 +1,41 @@
+//fetching data
+async function getMovie(userInput) {
+  const getApi = await fetch(`http://www.omdbapi.com/?t=${userInput}&apikey=57d5e741`);
+  const apiObj = await getApi.json();
+  if (apiObj.Response === "True") {
+    return apiObj;
+  } else {
+    throw "Error: Movie not found";
+  }
+}
+
+const elements = {
+  userInput: document.querySelector("input"),
+  poster: document.querySelector(".poster"),
+  title: document.querySelector(".title"),
+  genere: document.querySelector(".genere"),
+  year: document.querySelector(".year"),
+  plot: document.querySelector(".plot"),
+  director: document.querySelector(".director"),
+  actors: document.querySelector(".actors"),
+  rating: document.querySelector(".rating"),
+};
+
+//user search for movie
+document.querySelector("button").addEventListener("click", async () => {
+  const { userInput, poster, title, genere, year, plot, director, actors, rating } = elements;
+  const movie = await getMovie(userInput.value);
+  //   getMovie(userInput.value).catch((e) => console.log(e));
+  poster.style.background = `url("${movie.Poster}") no-repeat center center/contain`;
+  title.innerHTML = `<h2>${movie.Title}</h2>`;
+  genere.innerHTML = "<b>Genere:</b> " + movie.Genre;
+  year.innerHTML = "<b>Year:</b> " + movie.Year;
+  plot.innerHTML = "<b>Plot:</b> " + movie.Plot;
+  director.innerHTML = "<b>Director:</b> " + movie.Director;
+  actors.innerHTML = "<b>Actors:</b> " + movie.Actors;
+  let strRating = "";
+  movie.Ratings.forEach((site) => {
+    strRating += "<b>" + site.Source + ":</b> " + site.Value + "</br>";
+  });
+  rating.innerHTML = "<b>Rating:</b><br> " + strRating;
+});
