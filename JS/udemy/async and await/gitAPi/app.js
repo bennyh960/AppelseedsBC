619d597e15dfc8c76f89eefbdef9d6db3fd6079e
bennyh960 <bennyh960@gmail.com>
2022-05-10 13:55:15 +0300

    git api

--- JS/udemy/async and await/gitAPi/app.js
+++ JS/udemy/async and await/gitAPi/app.js
@@ -1,21 +1,65 @@
-// const url = "https://api.github.com/users/%7Bbennyh960%7D";
-// const url = "https://api.github.com/users/{bennyh960}";
-const url = "https://docs.github.com/rest/reference/users#bennyh960";
+const inputSearch = document.querySelector("input");
+const profiles = [];
 
-fetch(url)
-  .then((res) => {
-    // const data = res.json();
-    return res.headers;
-  })
-  .then((data) => console.log(data))
-  .catch((e) => console.log(e));
+async function getData() {
+  const api = await fetch(`https://api.github.com/users/${inputSearch.value}`);
+  inputSearch.value = "";
+  if (api.status !== 200) {
+    throw "invalid username.";
+  } else {
+    const data = await api.json();
+    if (!profiles.includes(data.id)) {
+      profiles.push(data.id);
+      return {
+        userImgUrl: data.avatar_url,
+        userName: data.login,
+        numOfRepo: data.public_repos,
+        userID: data.id,
+      };
+    }
+  }
+}
 
-// async function getUser() {
-//   try {
-//     const response = await axios.get(url);
-//     console.log(response);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-// getUser();
+function createElements() {
+  //create elements
+  const containerProfile = document.createElement("div");
+  const img = document.createElement("div");
+  const userNameClass = document.createElement("div");
+  const userNumOfRepo = document.createElement("div");
+  //append classes
+  containerProfile.classList.add("container");
+  img.classList.add("userImg");
+  userNameClass.classList.add("userName");
+  userNumOfRepo.classList.add("userNumOfRepo");
+  //append to container
+  containerProfile.append(img, userNameClass, userNumOfRepo);
+  //append to big container
+  document.querySelector(".all-profiles").append(containerProfile);
+  return {
+    img: img,
+    userNameClass: userNameClass,
+    userNumOfRepo: userNumOfRepo,
+    container: containerProfile,
+    containerAll: document.querySelector(".all-profiles"),
+  };
+}
+
+document.querySelector("button").addEventListener("click", (e) => {
+  const elementObj = createElements();
+  const { img, userNameClass, userNumOfRepo } = elementObj;
+  const dataObj = getData();
+  dataObj
+    .then((data) => {
+      img.style.background = `url("${data.userImgUrl}") no-repeat center center / cover`;
+      userNameClass.textContent = `Username : ${data.userName} `;
+      userNumOfRepo.textContent = `Number of public repo : ${data.numOfRepo} `;
+    })
+    .catch((e) => {
+      userNameClass.textContent = "Error : Invalid Profile";
+      userNumOfRepo.textContent = ` `;
+      img.style.background = `url("https://i1.sndcdn.com/avatars-000143401105-hcmrio-t500x500.jpg") no-repeat center center / cover`;
+      setTimeout(() => {
+        elementObj.containerAll.removeChild(elementObj.container);
+      }, 1500);
+    });
+});
